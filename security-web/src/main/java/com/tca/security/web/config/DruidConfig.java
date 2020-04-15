ac884a1dd4825482b6c2d87606cbc6ac3f83e461
zhoua <870235784@qq.com>
2020-04-16 00:19:57 +0800

    1.数据连接池基本配置

---
+++ security-web/src/main/java/com/tca/security/web/config/DruidConfig.java
@@ -0,0 +1,23 @@
+package com.tca.security.web.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * @author zhoua
+ * @Date 2020/4/14
+ * DruidDataSource配置
+ */
+@Configuration
+public class DruidConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource druid() {
+        return new DruidDataSource();
+    }
+}
---
+++ security-web/src/main/java/com/tca/security/web/config/MybatisPlusConfig.java
@@ -0,0 +1,27 @@
+/*
+package com.tca.security.web.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+*/
+/**
+ * @author zhoua
+ * @Date 2020/4/15
+ *//*
+
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.tca.security.web.mapper")
+public class MybatisPlusConfig {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        return paginationInterceptor;
+    }
+}
+*/
